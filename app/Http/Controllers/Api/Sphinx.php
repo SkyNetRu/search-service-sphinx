010286decf251cfb6283aca189053d928588c8d0
Nikolai Demin <demin.nikolai@inbox.ru>
2023-02-09 20:48:18 +0800

    'iniut'

---
+++ app/Http/Controllers/Api/Sphinx.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchRequest;
+use App\Models\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Foolz\SphinxQL\SphinxQL;
+use Foolz\SphinxQL\Drivers\Mysqli\Connection;
+
+class Sphinx extends Controller
+{
+    public function search (SearchRequest $request)
+    {
+        $conn = new Connection();
+        $conn->setParams([
+            'host' => config('database.connections.sphinx.host'),
+            'port' => config('database.connections.sphinx.port')
+        ]);
+
+        $query = (new SphinxQL($conn))->select('*')
+            ->from(config('database.connections.sphinx.database'))
+            ->match(['title', 'title_rus', 'introtext', 'introtext_rus'], $request->search_string)
+            ->where('sold', '=', 0)
+            ->where('published', '=', 1);
+
+        $result = $query->execute();
+        $result = collect($result->fetchAllAssoc())->pluck('id');
+
+        $items = Item::whereIn('id', $result)->get();
+        dump('search query - '.$request->search_string);
+        dd($items->pluck('title'));
+    }
+}
---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use Illuminate\Contracts\Validation\Validator;
+
+
+class SearchRequest extends FormRequest
+
+{
+
+    public function rules()
+    {
+        return [
+            'search_string' => 'required|max:255'
+        ];
+
+    }
+
+
+    public function failedValidation(Validator $validator)
+    {
+
+        throw new HttpResponseException(response()->json([
+
+            'success' => false,
+
+            'message' => 'Validation errors',
+
+            'data' => $validator->errors()
+
+        ]));
+    }
+
+
+    public function messages()
+    {
+        return [
+            'search_string.required' => 'Search string is required'
+        ];
+
+    }
+
+}
